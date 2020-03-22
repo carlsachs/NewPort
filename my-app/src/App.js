18d6187d08d80bb10fb981cccf2bab1d3695e313
carlsachs <sachscarl@gmail.com>
2020-03-22 12:03:04 -0400

    added dependencies, added particles, started styling landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from "./components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Home />
     </div>
   );
 }
