aa42bf27f859e03e01d143efde0e7e18eb3f82f3
carlsachs <sachscarl@gmail.com>
2020-03-24 23:31:23 -0400

    progress on first half of highlight projects on unit 1

--- my-app/src/components/projects/Unit1.js
+++ my-app/src/components/projects/Unit1.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+// Import Pictures
+
+import Flexbox from "./Flexbox.png";
+
 // STYLING
 
 import styled from "styled-components";
@@ -11,6 +15,7 @@ const Card = styled.div`
     flex-flow: column nowrap;
     margin: 0 auto;
     margin-top: 3%;
+    margin-bottom: 5%;
     box-shadow: 0 14px 28px rgba(11,22,84,0.25), 0 10px 10px rgba(11, 22, 84,0.22);
     `;
 
@@ -27,6 +32,7 @@ const Card = styled.div`
         flex-flow: row nowrap;
         justify-content: space-between;
         width: 100%;
+        color: #303D84;
         h3 {
             text-align: center;
             margin-left: 30%;
@@ -34,6 +40,49 @@ const Card = styled.div`
         ul {
             margin-right: 30%;
         }
+    `;
+
+    const High = styled.div`
+        width: 100%;
+        border: 1px solid blue;
+        color: #303D84;
+        h2 {
+            display: flex;
+            justify-content: center;
+        }
+    `;
+    const Left = styled.div`
+        display: flex;
+        border: 1px solid green;
+        width: 40%;
+        justify-content: space-evenly;
+        h3 {
+            margin: 0 auto;
+            margin-top: 10%;
+            margin-bottom: 10%;
+        }
+        img {
+            height: 150px;
+            :hover {
+                box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+            }
+        }
+        a {
+            margin: 0 auto;
+            margin-bottom: 10%;
+            text-decoration: none;
+            border: 1px solid blue;
+            width: 70%;
+            height: 35px;
+            text-align: center;
+            padding-top: 5%;
+            border-radius: 5%;
+        }
+    `;
+
+    const Flex = styled.div`
+        display: flex;
+        flex-flow: column nowrap;
     `;
 
 const Unit1 = () => {
@@ -48,7 +97,16 @@ const Unit1 = () => {
                     <li>GitFlow</li>
                 </ul>
             </Focus>
-            
+            <High>
+                <h2>Highlight Projects</h2>
+                <Left>
+                    <Flex>
+                        <h3>FlexBox</h3>
+                        <a href='https://github.com/carlsachs/UI-III-Flexbox'>Github Repo</a>
+                        <img src={Flexbox}/>
+                    </Flex>
+                </Left>
+            </High>
         </Card>
     )
 }
