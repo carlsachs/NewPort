c018111c98de7bf91b12289bf979923db513b748
carlsachs <sachscarl@gmail.com>
2020-03-24 22:52:18 -0400

    finished focus section of unit1

--- my-app/src/components/projects/Unit1.js
+++ my-app/src/components/projects/Unit1.js
@@ -6,9 +6,9 @@ import styled from "styled-components";
 
 const Card = styled.div`
     width: 80%;
-    height: 20vh;
     background-color: #white;
     display: flex;
+    flex-flow: column nowrap;
     margin: 0 auto;
     margin-top: 3%;
     box-shadow: 0 14px 28px rgba(11,22,84,0.25), 0 10px 10px rgba(11, 22, 84,0.22);
@@ -24,24 +24,31 @@ const Card = styled.div`
 
     const Focus = styled.div`
         display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        width: 100%;
+        h3 {
+            text-align: center;
+            margin-left: 30%;
+        }
+        ul {
+            margin-right: 30%;
+        }
     `;
-
-    const Right = styled.div``;
-
-    const Left = styled.div``;
 
 const Unit1 = () => {
     return (
         <Card>
             <Title>Unit 1: Web Fundamentals</Title>
             <Focus>
-                <Left>
-                
-                </Left>
-                <Right>
-                
-                </Right>
+                <h3>Focus:</h3>
+                <ul>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>GitFlow</li>
+                </ul>
             </Focus>
+            
         </Card>
     )
 }
