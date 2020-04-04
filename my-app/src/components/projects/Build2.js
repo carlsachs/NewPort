b0ec0221e82c307011e5c1d2b4cba6e657dd9348
carlsachs <sachscarl@gmail.com>
2020-04-04 13:15:04 -0400

    Build one 1280px done

--- my-app/src/components/projects/Build2.js
+++ my-app/src/components/projects/Build2.js
@@ -34,6 +34,10 @@ import styled from "styled-components";
             :hover {
                 box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
             }
+            @media all and (max-width:1280px){
+                height: 50px;
+                padding-top: .75%;
+            }
         }
         h2 {
             margin: 0 auto;
