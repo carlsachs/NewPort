ee83e1815ba30fc749de31db4883f535b44399d0
carlsachs <sachscarl@gmail.com>
2020-04-05 14:27:11 -0400

    unit 7 800 px finished:

--- my-app/src/components/projects/Unit7.js
+++ my-app/src/components/projects/Unit7.js
@@ -28,6 +28,9 @@ const Card = styled.div`
         height: 25%;
         margin: 0 auto;
         margin-top: 2%;
+        @media all and (max-width: 800px){
+            text-align: center;
+        }
     `;
 
     const Focus = styled.div`
@@ -63,9 +66,8 @@ const Card = styled.div`
         color: #303D84;
         display: flex;
         justify-content: space-between;
-        h2 {
-            display: flex;
-            justify-content: center;
+        @media all and (max-width: 800px){
+            flex-flow: column nowrap;
         }
     `;
     const Left = styled.div`
@@ -78,6 +80,10 @@ const Card = styled.div`
         background: rgb(23,69,91);
         background: radial-gradient(circle, rgba(23,69,91,0.5676645658263305) 0%, rgba(8,31,94,0.4920343137254902) 100%);
         box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        @media all and (max-width: 800px){
+            width: 90%;
+            height: 125px;
+        }
         h3 {
             margin: 0 auto;
             margin-top: 10%;
@@ -88,6 +94,9 @@ const Card = styled.div`
             @media all and (max-width:1280px){
                 width: 80%;
                 margin: 0 auto;
+            }
+            @media all and (max-width: 800px){
+                display: none;
             }
         }
         a {
@@ -103,53 +112,74 @@ const Card = styled.div`
             :hover {
                 box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
             }
+            @media all and (max-width: 800px){
+                width: 100%;
+                padding-top: 6%;
+            }
         }
     `;
 
     const Right = styled.div`
-    display: flex;
-    width: 40%;
-    justify-content: space-evenly;
-    margin-top: 2%;
-    margin-right: 5%;
-    margin-bottom: 2%;
-    padding-bottom: 2%;
-    background: rgb(23,69,91);
-    background: radial-gradient(circle, rgba(23,69,91,0.5676645658263305) 0%, rgba(8,31,94,0.4920343137254902) 100%);
-    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-    h3 {
-        margin: 0 auto;
-        margin-top: 12%;
-        margin-bottom: 12%;
-    }
-    img {
-        height: 150px;
-    }
-    a {
-        margin: 0 auto;
-        margin-bottom: 12%;
-        margin-top: 1%;
-        text-decoration: none;
-        border: 1px solid #0B1654;
-        width: 100%;
-        height: 35px;
-        text-align: center;
-        padding-top: 5%;
-        border-radius: 5%;
-        :hover {
-            box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+        display: flex;
+        width: 40%;
+        justify-content: space-evenly;
+        margin-top: 2%;
+        margin-right: 5%;
+        margin-bottom: 2%;
+        padding-bottom: 2%;
+        background: rgb(23,69,91);
+        background: radial-gradient(circle, rgba(23,69,91,0.5676645658263305) 0%, rgba(8,31,94,0.4920343137254902) 100%);
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        @media all and (max-width: 800px){
+            width: 90%;
+            height: 125px;
+            margin: 0 auto;
+            margin-bottom: 2%;
         }
-    }
+        h3 {
+            margin: 0 auto;
+            margin-top: 12%;
+            margin-bottom: 12%;
+            @media all and (max-width: 800px){
+                margin-top: 6.5%;
+                margin-bottom: 9%;
+            }
+        }
+        img {
+            height: 150px;
+            @media all and (max-width: 800px){
+                display: none;
+            }
+        }
+        a {
+            margin: 0 auto;
+            margin-bottom: 12%;
+            margin-top: 1%;
+            text-decoration: none;
+            border: 1px solid #0B1654;
+            width: 100%;
+            height: 35px;
+            text-align: center;
+            padding-top: 5%;
+            border-radius: 5%;
+            :hover {
+                box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+            }
+            @media all and (max-width: 800px){
+                width: 68%;
+                padding-bottom: 4%;
+            }
+        }
     `;
 
     const Flex = styled.div`
-    display: flex;
-    flex-flow: column nowrap;
+        display: flex;
+        flex-flow: column nowrap;
     `;
 
     const Flex1 = styled.div`
-    display: flex;
-    flex-flow: column nowrap;
+        display: flex;
+        flex-flow: column nowrap;
     `;
 
 const Unit7 = () => {
