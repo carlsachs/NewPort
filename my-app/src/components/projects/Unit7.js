ed856caffdb723cbf25447cf0b0ccc04ed6d21d4
carlsachs <sachscarl@gmail.com>
2020-04-05 22:39:59 -0400

    unit 7 360px done

--- my-app/src/components/projects/Unit7.js
+++ my-app/src/components/projects/Unit7.js
@@ -20,6 +20,9 @@ const Card = styled.div`
     background: rgb(53,149,255);
     background: radial-gradient(circle, rgba(53,149,255,0.15449929971988796) 0%, rgba(240,244,255,0.6587009803921569) 100%);
     box-shadow: 0 14px 28px rgba(11,22,84,0.25), 0 10px 10px rgba(11, 22, 84,0.22);
+    @media all and (max-width: 360px){
+        padding-bottom: 3%;
+    }
     `;
 
     const Title = styled.h1`
@@ -31,6 +34,9 @@ const Card = styled.div`
         @media all and (max-width: 800px){
             text-align: center;
             margin-top: 3%;
+        }
+        @media all and (max-width: 360px){
+            font-size: 1.25rem;
         }
     `;
 
@@ -48,11 +54,17 @@ const Card = styled.div`
         @media all and (max-width: 500px){
             flex-direction: column;
         }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
+        }
     `;
 
     const FocTitle = styled.div`
     @media all and (max-width: 500px){
         margin: 0 auto;
+    }
+    @media all and (max-width: 360px){
+        margin-top: 4%;
     }
         h3 {
             margin-top: 40%;
@@ -68,6 +80,9 @@ const Card = styled.div`
             padding-left: 3%;
             padding-right: 1%;   
         }
+        @media all and (max-width: 360px){
+            margin-left: 1%;
+        }
     `;
 
     const HighlightTitle = styled.div`
@@ -76,6 +91,10 @@ const Card = styled.div`
         margin-bottom: 1%;
         color: #303D84;
         @media all and (max-width: 500px){
+            margin-left: 25%;
+        }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
             margin-left: 25%;
         }
     `;
@@ -87,6 +106,9 @@ const Card = styled.div`
         justify-content: space-between;
         @media all and (max-width: 800px){
             flex-flow: column nowrap;
+        }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
         }
     `;
     const Left = styled.div`
@@ -102,6 +124,9 @@ const Card = styled.div`
         @media all and (max-width: 800px){
             width: 90%;
             height: 125px;
+        }
+        @media all and (max-width: 360px){
+            height: 90px;
         }
         h3 {
             margin: 0 auto;
@@ -135,6 +160,9 @@ const Card = styled.div`
                 width: 100%;
                 padding-top: 6%;
             }
+            @media all and (max-width: 360px){
+                padding-bottom: 6%;
+            }
         }
     `;
 
@@ -155,6 +183,9 @@ const Card = styled.div`
             margin: 0 auto;
             margin-bottom: 2%;
         }
+        @media all and (max-width: 360px){
+            height: 90px;
+        }
         h3 {
             margin: 0 auto;
             margin-top: 12%;
@@ -162,6 +193,9 @@ const Card = styled.div`
             @media all and (max-width: 800px){
                 margin-top: 6.5%;
                 margin-bottom: 9%;
+            }
+            @media all and (max-width: 360px){
+                margin-bottom: 8%;
             }
         }
         img {
