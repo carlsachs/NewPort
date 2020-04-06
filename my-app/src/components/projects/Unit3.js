bac2d55fe445801b3f62b6392f9041f1c1840852
carlsachs <sachscarl@gmail.com>
2020-04-05 21:47:38 -0400

    unit 3 360px done

--- my-app/src/components/projects/Unit3.js
+++ my-app/src/components/projects/Unit3.js
@@ -21,6 +21,9 @@ const Card = styled.div`
         background: rgb(53,149,255);
         background: radial-gradient(circle, rgba(53,149,255,0.15449929971988796) 0%, rgba(240,244,255,0.6587009803921569) 100%);
         box-shadow: 0 14px 28px rgba(11,22,84,0.25), 0 10px 10px rgba(11, 22, 84,0.22);
+        @media all and (max-width: 360px){
+            padding-bottom: 3%;
+        }
     `;
 
     const Title = styled.h1`
@@ -32,6 +35,9 @@ const Card = styled.div`
         @media all and (max-width: 500px){
             text-align: center;
             margin-top: 3%;
+        }
+        @media all and (max-width: 360px){
+            font-size: 1.25rem;
         }
     `;
 
@@ -50,6 +56,9 @@ const Card = styled.div`
         @media all and (max-width: 500px){
             flex-direction: column;
         }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
+        }
     `;
 
     const FocTitle = styled.div`
@@ -66,7 +75,11 @@ const Card = styled.div`
         }
     `;
 
-    const List = styled.div``;
+    const List = styled.div`
+    @media all and (max-width: 360px){
+        margin-right: 10%;
+    }
+    `;
 
     const HighlightTitle = styled.div`
         display: flex;
@@ -74,6 +87,10 @@ const Card = styled.div`
         margin-bottom: 1%;
         color: #303D84;
         @media all and (max-width: 500px){
+            margin-left: 25%;
+        }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
             margin-left: 25%;
         }
     `;
@@ -90,6 +107,9 @@ const Card = styled.div`
         @media all and (max-width: 800px){
             flex-flow: column nowrap;
         }
+        @media all and (max-width: 360px){
+            font-size: .75rem;
+        }
     `;
     const Left = styled.div`
         display: flex;
@@ -105,6 +125,9 @@ const Card = styled.div`
             width: 90%;
             height: 125px;
             margin-bottom: .50%;
+        }
+        @media all and (max-width: 360px){
+            height: 90px;
         }
         h3 {
             margin: 0 auto;
@@ -153,6 +176,11 @@ const Card = styled.div`
                 padding-top: 3%;
                 padding-bottom: 3%;
             }
+            @media all and (max-width: 360px){
+                width: 51%;
+                padding-right: 3%;
+                padding-left: 3%;
+            }
         }
     `;
 
@@ -171,6 +199,9 @@ const Card = styled.div`
         @media all and (max-width: 800px){
             width: 90%;
             height: 125px;
+        }
+        @media all and (max-width: 360px){
+            height: 90px;
         }
         h3 {
             margin: 0 auto;
@@ -218,6 +249,10 @@ const Card = styled.div`
                 width: 60%;
                 margin-top: 5%;
             }
+            @media all and (max-width: 360px){
+                padding-right: 3%;
+                padding-left: 3%;
+            }
         }
 `;
 
@@ -237,6 +272,9 @@ const Card = styled.div`
             height: 125px;
             margin: 0 auto;
             margin-bottom: 2%;
+        }
+        @media all and (max-width: 360px){
+            height: 90px;
         }
         h3 {
             margin: 0 auto;
@@ -283,6 +321,10 @@ const Card = styled.div`
                 padding-top: 6%;
                 padding-bottom: 6%;
                 margin-top: 1%;
+            }
+            @media all and (max-width: 360px){
+                padding-right: 3%;
+                padding-left: 3%;
             }
         }
     `;
